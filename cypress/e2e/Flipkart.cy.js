b866180f593be38f3fc274ec567dedb38acb5f50
ETEC\AkashKumar <akashkumar@policybazaar.com>
2023-08-13 01:05:40 +0530

    iphone search

--- cypress/e2e/Flipkart.cy.js
+++ cypress/e2e/Flipkart.cy.js
@@ -6,9 +6,10 @@ describe('Flipkart test suite', () => {
     cy.title().should('eq', 'Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!');
   });
 
-  it('FT1', () => {
-    cy.visit('https://www.flipkart.com/')
-    cy.title().should('eq','Flipkart')
+  it('FT2', () => {
+    cy.get('._3704LK').type('i phone 12')
+    cy.get('.L0Z3Pu').click()
+    cy.xpath('//*[@id="container"]/div/div[3]/div[1]/div[2]/div[2]/div/div/div/a').invoke('removeAttr', 'target').click()
   })
 
 })
