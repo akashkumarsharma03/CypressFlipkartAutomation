0c7db2db66f0af620793a232d74def123c2b7b22
Akash Kumar <iamakash0326@gmail.com>
2023-08-12 13:09:57 +0530

    Update Flipkart.cy.js

--- cypress/e2e/Flipkart.cy.js
+++ cypress/e2e/Flipkart.cy.js
@@ -2,7 +2,7 @@ describe('Flipkart test suit', () => {
 
   it('FT1', () => {
     cy.visit('https://www.flipkart.com/')
-    cy.title().should('eq','Flipkart')
+    cy.title().should('eq','Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!')
   })
 
 
